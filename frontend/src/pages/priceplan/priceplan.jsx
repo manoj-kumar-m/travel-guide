20af9de75983e93f336468558c56f02dc1ba1b24
1ms19cs066 <99071162+1ms19cs066@users.noreply.github.com>
2022-05-18 19:08:00 +0530

    Update priceplan.jsx

--- frontend/src/pages/priceplan/priceplan.jsx
+++ frontend/src/pages/priceplan/priceplan.jsx
@@ -4,7 +4,48 @@ const Priceplan = () => {
   return (
       <>
         <div className={classes.container}>
-              
+              <h2>Choose your plan</h2>
+        <div className={classes.price-row}>
+            <div className={classes.price-col}>
+                <p>Starter</p>
+                <h3>19$ <span>/ month</span></h3>
+                <ul>
+                    <li>1 Website</li>
+                    <li>10 GB Disk Space</li>
+                    <li>Free Email Address</li>
+                    <li>Basic Web Builder</li>
+                    <li>No SSL Certificate</li>
+                    <li>Limited Support</li>
+                </ul>
+                <button>Add To Cart</button>
+            </div>
+            <div className={classes.price-col}>
+                <p>Advanced</p>
+                <h3>19$ <span>/ month</span></h3>
+                <ul>
+                    <li>1 Website</li>
+                    <li>10 GB Disk Space</li>
+                    <li>Free Email Address</li>
+                    <li>Basic Web Builder</li>
+                    <li>No SSL Certificate</li>
+                    <li>Limited Support</li>
+                </ul>
+                <button>Add To Cart</button>
+            </div>
+            <div className={classes.price-col}>
+                <p>Premium</p>
+                <h3>19$ <span>/ month</span></h3>
+                <ul>
+                    <li>1 Website</li>
+                    <li>10 GB Disk Space</li>
+                    <li>Free Email Address</li>
+                    <li>Basic Web Builder</li>
+                    <li>No SSL Certificate</li>
+                    <li>Limited Support</li>
+                </ul>
+                <button>Add To Cart</button>
+            </div>
+        </div>
         </div>
 
       </>
